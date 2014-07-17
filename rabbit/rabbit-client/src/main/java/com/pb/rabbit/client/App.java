45638a6d51fc43538f21d8363ff99c3e10f3d3d3
danny314 <danny.explorer@gmail.com>
2014-07-17 11:05:54 -0500

    Externalized broker properties.

--- rabbit/rabbit-client/src/main/java/com/pb/rabbit/client/App.java
+++ rabbit/rabbit-client/src/main/java/com/pb/rabbit/client/App.java
@@ -1,31 +1,51 @@
 package com.pb.rabbit.client;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 /**
- * Hello world!
- *
+ * Client publish messages to an exchange on a remote rabbit mq broker. 
  */
 public class App 
 {
+    Properties props;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("172.17.0.2");
+        
+        App app = new App();
+        
+        factory.setHost(app.getProperty("broker.host"));
+        factory.setUsername(app.getProperty("broker.username"));
+        factory.setPassword(app.getProperty("broker.password"));
         
         Connection connection = null;
         
 		try {
 			connection = factory.newConnection();
 	        Channel channel = connection.createChannel();
+	        System.out.println( "Obtained channel " + channel);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+    }
+    
+    private String getProperty(String key) {
+    	if (props == null) {
+    		props = new Properties();
+    		
+            try {
+    			props.load(App.class.getClassLoader().getResourceAsStream("broker.properties"));
+    		} catch (IOException e1) {
+    			e1.printStackTrace();
+    		}
+            
+    	}
+    	return props.getProperty(key);
     }
 }

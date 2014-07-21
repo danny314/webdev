efbd777ab5cd6d5a0b8b4061b0d4cb5f12016818
danny314 <danny.explorer@gmail.com>
2014-07-21 13:44:58 -0500

    Submitting multiple fields.

--- enroll-rest/rest-client.sh
+++ enroll-rest/rest-client.sh
@@ -1,9 +1,13 @@
 #!/bin/bash 
          COUNTER=0
          date1=$(date +"%s")
-         while [  $COUNTER -lt 1 ]; do
-         randomName=$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)
-curl -H "Content-Type: application/json" -d '{"name":"'"$randomName"'"}' http://localhost:8080/enroll-rest-1.0-SNAPSHOT/api/public/enroll           
+         while [  $COUNTER -lt 10000 ]; do
+curl -H "Content-Type: application/json" -d '{ 
+								"name":"'"$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)"'" 
+								,"email":"'"$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)"'" 
+								,"username":"'"$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)"'" 
+								,"gender":"F" 
+								}' http://localhost:8080/enroll-rest-1.0-SNAPSHOT/api/public/enroll           
              let COUNTER=COUNTER+1 
          done
          date2=$(date +"%s")
